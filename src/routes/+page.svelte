12ff44def735db35219bf093ac9bc7c63121e4be
gargonjj <jesuramone@gmail.com>
2026-05-22 12:14:56 +0100

    a

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,7 @@
     let { data } = $props();
     const MONTHS = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 
+
 </script>
 
 <div class="w-full h-full bg-gray-400 p-[20px]">
@@ -19,11 +20,13 @@
         {#each data.activities as activity}
             <a href={`/sessions/${activity.id}`} class="block m-[10px] p-[10px] hover:scale-105 transition 
                 duration-300 active:bg-blue-300 active:scale-110 bg-gray-300 rounded-[3mm]">
-                <span class="text-[20px] sm:text-[24px] active:scale-110 font-bold"
-                >{activity.activity_name}</span>
-                <hr>
-                <span class="text-[16px] sm:text-[18px] italic"
-                >{activity.day} de {MONTHS[activity.month - 1]} de {activity.year}</span>
+                <div class="ml-2 mr-2">
+                    <span class="text-[20px] sm:text-[24px] active:scale-110 font-bold"
+                    >{activity.activity_name}</span>
+                    <hr class="mt-2 mb-2">
+                    <span class="text-[16px] sm:text-[18px] italic"
+                    >{activity.day} de {MONTHS[activity.month - 1]} de {activity.year}</span>
+                </div>
                 <img src={`${import.meta.env.VITE_TICKETARY_API}${JSON.parse(activity.url)['original']}`} 
                 alt={activity.activity_name} class="w-full h-[300px] object-cover rounded-[3mm] mt-[10px]" />
             </a>
