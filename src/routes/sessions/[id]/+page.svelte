4fe43607c71cf02d37e15eae9dfd5ea5d8eb1608
gargonjj <jesuramone@gmail.com>
2026-05-22 10:42:48 +0100

    actualiza css [id]

--- src/routes/sessions/[id]/+page.svelte
+++ src/routes/sessions/[id]/+page.svelte
@@ -64,18 +64,16 @@
         <img src='{img}' alt="Fondo" class="w-full h-full object-cover block blur scale-110"/>
     </div>
 
-    <div id="container" class="w-screen 2xl:w-[40%] bg-[#fbfbfb] rounded-[3mm] relative m-[0_auto] justify-center z-20 p-right-5 p-left-5">
-        <div id="cabecera" class="w-full bg-amber-200 h-[850px] 2xl:w-full flex justify-center items-center">
-            <img src='{img}' alt='Portada de "El postre"' class = " 2xl:w-[50%] rounded-[1mm]"/>
+    <div id="container" class="w-screen 2xl:w-[40%] bg-[#fbfbfb] rounded-lg relative mr-5 ml-5 justify-center z-20 p-right-5 p-left-5">
+        <div id="cabecera" class="w-full h-full 2xl:w-full flex justify-center items-center">
+            <img src='{img}' alt='Portada de "El postre"' class = " w-fit rounded-[1mm]"/>
         </div>
 
-        <div id="titulo" class="grid grid-cols-[60%_40%] max-2xl:grid-cols-1 items-center justify-center bg-[#fbfbfb] rounded-[5mm] rounded-tl-none rounded-tr-none gap-5">
-            <div id="titulo_info" class="ml-5 justify-around conten-center p-4">
-                <h1 class="text-xl font-bold">{activityInfo.day} de {months[activityInfo.month - 1]}</h1>
-                <h1 class="text-2xl font-bold">{activityInfo.activity_name}</h1>
-                <div id="ubiprecio" class="grid grid-cols-[50fr_50fr] gap-5">
-                    <a
-                        class="text-opacity-60 text-black text-[22px] content-center"
+        <div id="titulo" class="grid grid-cols-[60%_40%] max-2xl:grid-cols-1 items-center justify-center mr-2 ml-2 mt-2 lg:bg-gray-200 lg:rounded-lg" >
+            <div id="titulo_info" class="justify-around conten-center p-4 bg-gray-200 rounded-lg gap-5 mb-2">
+                <h1 class="text-l font-bold">{activityInfo.day} de {months[activityInfo.month - 1]}</h1>
+                <a
+                        class="text-opacity-60 text-black text-[14px] content-center self-end italic"
                         id="ubicacion"
                         bind:this={ubi}
                         href="https://duckduckgo.com/?q=teatro+cine+los+realejos&iaxm=maps&source=placesy"
@@ -84,16 +82,18 @@
                     >
                         {activityInfo.places_name}
                     </a>
+                <h1 class="text-3xl font-bold mt-1 mb-1">{activityInfo.activity_name}</h1>
+                <div id="ubiprecio" class="grid grid-cols-[1fr] gap-5">
                     {#if activityInfo.amount === 0}
-                        <b class="text-2xl m-[0_auto] content-center">Gratuito</b>
+                        <b class="text-xl font-normal">Gratuito</b>
                     {:else}
-                        <b class="text-2xl m-[0_auto]">{activityInfo.amount}€</b>
+                        <b class="text-xl font-normal">{activityInfo.amount}€</b>
                     {/if}
                 </div>
             </div>
             <div id="buttonBox" class="flex items-center content-center justify-center p-4">
                 <button
-                    class="bg-[#b73434] hover:bg-[#c75b5b]  text-white text-[20px] font-bold py-3 px-12 margin-right-[5px] rounded"
+                    class="bg-[#5a1d89] hover:bg-[#7d3ead] lg:hover:underline  text-white text-[20px] font-bold py-3 px-12 margin-right-[5px] rounded-lg"
                     id="entradas"
                     bind:this={boton}
                 >
@@ -101,37 +101,37 @@
                 </button>
             </div>
         </div>
-
-        <div class="flex flex-col 2xl:grid 2xl:grid-cols-[70%_30%] gap-10 max-w-225 mx-auto">
-        <div class="margin-left-[20px] p-4" >
+        <hr class="m-2 ml-5 mr-5 opacity-30"/>
+        <div class="flex flex-col 2xl:grid 2xl:grid-cols-[70%_30%] max-w-225 mx-auto">
+        <div class="ml-5 mr-5 p-4 **:font-sans!" >
             <div>{@html activityInfo.description}</div>
         </div>
 
-        <div class="margin-right-[60px_0] 2xl:margin-right-[20px] 2xl:w-[75%] 2xl:col-start-2 p-2" id="data">
-            <div class="margin-right-[10px] flex">
-                <img class="w-5 h-5 margin-right-[0.5em] icon float-left" src="/calendar-dots.png" alt="Icono calendario" />
-                <h4 class="text-[16px] font-bold">Fecha y Hora</h4>
+        <div class="ml-5 mr-5 2xl:margin-right-[20px] 2xl:w-[75%] 2xl:col-start-2 p-2" id="data">
+            <div class="margin-right-[10px] mt-2 flex ">
+                <img class="w-8 h-8 mr-1 icon float-left self-center" src="/calendar-dots.png" alt="Icono calendario" />
+                <h4 class="text-[16px] font-bold self-center">Fecha y Hora</h4>
             </div>
-            <p>{days[activityInfo.dow - 1]}, {activityInfo.day} de {months[activityInfo.month -1]} de {activityInfo.year}, {hours}:{minutes}</p>
-            <hr />
+            <p>{days[activityInfo.dow - 1]}, {activityInfo.day} de {months[activityInfo.month -1]} de {activityInfo.year}, {hours}:{minutes ? minutes : '00'}</p>
+            <hr class="mt-1"/>
 
-            <div class="margin-right-[10px] flex">
-                <img class="w-5 h-5 margin-right-[0.5em] icon" src="/map-pin.png" alt="Icono lugar" />
-                <h4 class="text-[16px] font-bold float-left">Lugar</h4>
+            <div class="margin-right-[10px] mt-2 flex ">
+                <img class="w-8 h-8 mr-1 icon self-center" src="/map-pin.png" alt="Icono lugar" />
+                <h4 class="text-[16px] font-bold float-left self-center">Lugar</h4>
             </div>
             <p>{activityInfo.places_name}</p>
-            <hr />
+            <hr class="mt-1" />
 
-            <div class="margin-right-[10px] flex">
-                <img class="w-5 h-5 margin-right-[0.5em] icon" src="/users-three.png" alt="Icono clasificación" />
-                <h4 class="text-[16px] font-bold float-left"   >Clasificación</h4>
+            <div class="margin-right-[10px] mt-2 flex">
+                <img class="w-8 h-8 mr-1 icon self-center" src="/users-three.png" alt="Icono clasificación" />
+                <h4 class="text-[16px] font-bold float-left self-center"   >Clasificación</h4>
             </div>
             <p>{activityInfo.public_name}</p>
-            <hr />
+            <hr class="mt-1" />
 
-            <div class="margin-right-[10px] flex">
-                <img class="w-5 h-5 margin-right-[0.5em] icon" src="/file-text.png" alt="Icono reembolso" />
-                <h4 class="text-[16px] font-bold float-left">Políticas de reembolso</h4>
+            <div class="margin-right-[10px] mt-2 flex">
+                <img class="w-8 h-8 mr-1 icon self-center" src="/file-text.png" alt="Icono reembolso" />
+                <h4 class="text-[16px] font-bold float-left self-center">Políticas de reembolso</h4>
             </div>
             <p>
                 Contacta vía teléfono al 922 346 234
@@ -140,7 +140,7 @@
                 <br />
                 Avenida de Canarias, 6 CP38419, Los Realejos
             </p>
-            <hr />
+            <hr class="mt-1" />
         </div>
         </div>
     </div>
