b44941a3af6d7f1c3827aec7121ec745476e85e1
Jorgemarnes <queque-privado@protonmail.com>
2026-06-09 09:54:19 +0100

    update

--- .svelte-kit/non-ambient.d.ts
+++ .svelte-kit/non-ambient.d.ts
@@ -29,7 +29,7 @@ declare module "$app/types" {
 	type MatcherParam<M> = M extends (param : string) => param is (infer U extends string) ? U : string;
 
 	export interface AppTypes {
-		RouteId(): "/" | "/api" | "/api/activities" | "/api/coupons" | "/api/rooms" | "/sessions" | "/sessions/[id]";
+		RouteId(): "/" | "/api" | "/api/activities" | "/api/coupons" | "/api/rooms" | "/api/sessions" | "/sessions" | "/sessions/[id]";
 		RouteParams(): {
 			"/sessions/[id]": { id: string }
 		};
@@ -39,10 +39,11 @@ declare module "$app/types" {
 			"/api/activities": Record<string, never>;
 			"/api/coupons": Record<string, never>;
 			"/api/rooms": Record<string, never>;
+			"/api/sessions": Record<string, never>;
 			"/sessions": { id?: string };
 			"/sessions/[id]": { id: string }
 		};
-		Pathname(): "/" | "/api/activities" | "/api/coupons" | "/api/rooms" | `/sessions/${string}` & {};
+		Pathname(): "/" | "/api/activities" | "/api/coupons" | "/api/rooms" | "/api/sessions" | `/sessions/${string}` & {};
 		ResolvedPathname(): `${"" | `/${string}`}${ReturnType<AppTypes['Pathname']>}`;
 		Asset(): "/calendar-dots.png" | "/file-text.png" | "/foto.jpg" | "/map-pin.png" | "/robots.txt" | "/users-three.png" | string & {};
 	}
