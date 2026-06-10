10ac97a684ebcadbc7f03c66767806ce60992535
gargonjj <jesuramone@gmail.com>
2026-06-10 08:28:30 +0100

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,10 +30,10 @@
     }
 
     function toggleMenu() {
-        isOpen = !isOpen;   
+        isOpen = !isOpen;    
     }
 </script>
-    <div id="container" class="w-screen h-screen bg-[#f6f6f6] p-5 mx-0 overflow-x-hidden">
+    <div id="container" class="w-screen h-screen bg-[#f6f6f6] mx-0 overflow-x-hidden">
         <h1 class="flex p-5 mb-5 font-bold text-[45px] sm:text-[45px] sm:justify-center">Sesiones:</h1>
         <div class="mb-2.5 align-center 
             flex-wrap gap-4 bg-[#f6f6f6]  p-5  flex z-5">
@@ -45,7 +45,7 @@
                 transition:slide={{ duration: 300}}>
                 <label class=" self-center text-[16px]" for="fecha">Hasta:</label>
                 <input id="fecha" type="date" min="{min_date}" bind:value={filtroFecha}
-                class="border rounded bg-blue-50 pl-2 pr-2"/>
+                class="border rounded bg-blue-50"/>
                 <div class="basis-full"></div>
                 <button onclick={clearFiltros} class=" bg-gray-300 py-1 px-3 rounded">Limpiar filtros</button>
                 </div>
@@ -53,18 +53,18 @@
             </fieldset>
         </div>
 
-    <div class="justify-items-center align-center 
-    flex-wrap grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 
-    gap-4 bg-[#f6f6f6] p-5 mx-auto md:w-[90vw} xl:w-[70vw]">
+    <div class="justify-evenly align-center 
+    flex-wrap grid grid-cols-1 
+    gap-4 bg-[#f6f6f6] mx-auto px-20 md:w-[90vw} xl:w-screen xl:min-w-300">
         {#each eventosFiltrados as activity}
-            <a href={`/sessions/${activity.id}`} class="flex flex-col m-2.5 w-80 h-60 hover:scale-105 transition
-                duration-300 active:bg-blue-300 active:scale-110 bg-[#5a1d89] text-amber-50 hover:bg-black">
-                <div class="h-[60%] bg-amber-200 overflow-hidden ">
+            <a href={`/sessions/${activity.id}`} class="flex my-2.5 w-full xl:w-full h-60 hover:scale-105 transition
+                duration-300 active:bg-blue-300 active:scale-110 border-2 border-black hover:bg-black hover:text-white">
+                <div class="flex w-[20%] bg-[#5a1d89] overflow-hidden justify-center min-w-2xs hidden md:block">
                     <img src={`${import.meta.env.VITE_TICKETARY_API}${JSON.parse(activity.url || '{}')['medium']}`} 
-                    alt={activity.activity_name} class="w-full h-full object-cover scale-125" />
+                    alt={activity.activity_name} class="h-full w-full object-cover scale-125" />
                 </div>
-                <div class="ml-2 mr-2 h-[40%] pt-2 " >
-                    <span class="text-5 sm:text-[16px] active:scale-110 font-bold"
+                <div class="flex flex-col ml-2 mr-2 h-full py-5 px-2 justify-end" >
+                    <span class="text-5 text-[28px] active:scale-110 font-bold"
                     >{activity.activity_name}</span>
                     <hr class="mt-1 mb-1">
                     <span class="text-[16px] sm:text-[16px] italic"
