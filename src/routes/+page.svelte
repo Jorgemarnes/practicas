24977e13feffa1578ebb477cd10f0b27ebd7c6a4
Jorgemarnes <queque-privado@protonmail.com>
2026-06-10 08:47:28 +0100

    a???

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -45,29 +45,32 @@
     /---------------------------------------------------------------------------------------------------------------------/;
 
 </script>
-    <div class="w-screen h-screen bg-[#f6f6f6] p-5 mx-0 overflow-x-hidden">
+<div class="w-screen h-screen bg-[#ffffff] p-5 overflow-x-hidden">
+    <div class="">
     <!--------------------------------------------------------------------------------------------------------------------->
     <!--------------------------------------------------------------------------------------------------------------------->
     <!--------------------------------------------------------------------------------------------------------------------->
 
     <!--CREACION DE HEADING Y FILTRO POR FECHA-->
 
-        <h1 class="flex p-5 mb-5 font-bold text-[45px] sm:text-[45px] sm:justify-center">Sesiones:</h1>
-        <div class="mb-2.5 align-center 
-            flex-wrap gap-4 bg-[#f6f6f6]  p-5  flex z-5">
-            <fieldset>
-                <button class ="text-xl font-bold hover:underline p-2" onclick={toggleMenu}>Filtros</button>
+        <h1 class="flex p-5 mb-5 font-bold text-[45px] sm:text-[45px] sm:justify-center mx-20">Sesiones:</h1>
+        <div class="mb-2.5 align-center sm:justify-center
+            flex-wrap gap-4 bg-[#ffffff] p-5 flex z-5">
+            <fieldset class="flex flex-col w-full max-w-xs justify-center">
+                <button class ="text-xl font-bold hover:underline p-2 mx-20 hover:scale-110 transition duration-150 active:scale-95" onclick={toggleMenu}>Filtros</button>
                 {#if isOpen}
-                <div id ="filters" class="m-2.5 align-center 
-                flex-wrap gap-4 bg-[#f6f6f6] flex"
-                transition:slide={{ duration: 300}}>
-                <label class=" self-center text-[16px]" for="fecha">Hasta:</label>
-                <input id="fecha" type="date" min="{min_date}" bind:value={filtroFecha}
-                class="border rounded bg-blue-50 pl-2 pr-2"/>
-                <div class="basis-full"></div>
-                <button onclick={clearFiltros} class=" bg-gray-300 py-1 px-3 rounded">Limpiar filtros</button>
-                </div>
-                {/if}
+                    <div id ="filters" class="m-2.5 
+                    flex-wrap gap-4 bg-[#ffffff] flex mx-20 justify-center"
+                    transition:slide={{ duration: 300}}>
+                    <label class="self-center text-[16px]" for="fecha">Hasta:</label>
+                    <input id="fecha" type="date" min="{min_date}" bind:value={filtroFecha}
+                    class="border rounded bg-blue-50 pl-2 pr-2 hover:bg-gray-400 hover:text-white
+                    transition duration-150 hover:scale-108 hover:underline"/>
+                    <div class="basis-full"></div>
+                    <button onclick={clearFiltros} class=" bg-gray-300 py-1 px-3 rounded hover:bg-gray-400 hover:text-white
+                    hover:scale-110 active:scale-95 transition duration-150 active:bg-gray-600">Limpiar filtros</button>
+                    </div>
+                {/if} 
             </fieldset>
         </div>
     <!--------------------------------------------------------------------------------------------------------------------->
@@ -76,27 +79,29 @@
 
     <!--CREACION DE CADA SESION Y ORGANIZACION DE LA LISTA-->
 
-    <div class="justify-items-center align-center 
-    flex-wrap grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 
-    gap-4 bg-[#f6f6f6]  p-5">
-        {#each eventosFiltrados as activity}
-            <a href={`/sessions/${activity.id}`} class="flex flex-col m-2.5 w-80 h-60 hover:scale-105 transition
-                duration-300 active:bg-blue-300 active:scale-110 bg-[#5a1d89] text-amber-50 hover:bg-black">
-                <div class="h-[60%] bg-amber-200 overflow-hidden ">
-                    <img src={`${import.meta.env.VITE_TICKETARY_API}${JSON.parse(activity.url || '{}')['medium']}`} 
-                    alt={activity.activity_name} class="w-full h-full object-cover scale-125" />
-                </div>
-                <div class="ml-2 mr-2 h-[40%] pt-2 " >
-                    <span class="text-5 sm:text-[16px] active:scale-110 font-bold"
-                    >{activity.activity_name}</span>
-                    <hr class="mt-1 mb-1">
-                    <span class="text-[16px] sm:text-[16px] italic"
-                    >{activity.day} de {MONTHS[activity.month - 1]} de {activity.year}</span>
-                </div>
-            </a>
-        {/each}
+        <div class="justify-self-center self-center justify-items-center grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 
+        bg-[#ffffff] gap-5">
+            {#each eventosFiltrados as activity}
+                <a href={`/sessions/${activity.id}`} class="flex flex-col m-1 w-80 h-60 hover:scale-110 transition
+                    duration-150 active:bg-blue-300 active:scale-95 bg-[#5a1d89] text-amber-50 hover:bg-black border-black border-2">
+                    <div class="h-[60%] bg-amber-200 overflow-hidden">
+                        <img src={`${import.meta.env.VITE_TICKETARY_API}${JSON.parse(activity.url || '{}')['medium']}`} 
+                        alt={activity.activity_name} class="w-full h-full object-cover scale-125"/>
+                    </div>
+                    <div class="ml-2 mr-2 h-[40%] pt-2 grid row-span-3">
+                        <span class="text-5 sm:text-[16px] active:scale-110 font-bold h-5"
+                        >{activity.activity_name}</span>
+                        <div class="h-5 py-4">
+                            <hr/>
+                            <span class="text-[16px] sm:text-[16px] italic h-5"
+                            >{activity.day} de {MONTHS[activity.month - 1]} de {activity.year}</span>
+                        </div>
+                    </div>
+                </a>
+            {/each}
     <!--------------------------------------------------------------------------------------------------------------------->
     <!--------------------------------------------------------------------------------------------------------------------->
     <!--------------------------------------------------------------------------------------------------------------------->
+        </div>
     </div>
 </div>
